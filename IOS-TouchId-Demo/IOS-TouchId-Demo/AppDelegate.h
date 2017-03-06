2fa6e3f2e50358b8c8d72fb18f941cbd83de4a88
shuai <18112650981@163.com>
2017-03-06 08:59:59 +0800

    创建TouchID项目

---
+++ IOS-TouchId-Demo/IOS-TouchId-Demo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  IOS-TouchId-Demo
+//
+//  Created by 高帅 on 17/3/6.
+//  Copyright © 2017年 高帅. All rights reserved
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ IOS-TouchId-Demo/IOS-TouchId-Demo/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  IOS-TouchId-Demo
+//
+//  Created by 高帅 on 17/3/6.
+//  Copyright © 2017年 高帅. All rights reserved
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
